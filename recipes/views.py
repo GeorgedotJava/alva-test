4ec6e6ac4866152f7a2b4a321f200989d690d014
George Lucas <geolucas3@gmail.com>
2022-08-24 10:29:51 -0300

    primeiros ajustes do projeto

---
+++ recipes/views.py
@@ -0,0 +1,17 @@
+from http.client import HTTPResponse
+
+from django.http import HttpResponse
+
+# from django.shortcuts import render
+
+
+def home(request):
+    return HttpResponse('HOME')
+
+
+def sobre(request):
+    return HttpResponse('SOBRE')
+
+
+def contato(request):
+    return HttpResponse('CONTATO')
